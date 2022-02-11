b2d161579e5e13f06ab9ba95cf4a038299981572
NedaParvini <nedaparvini1@gmail.com>
2022-02-11 11:08:39 -0800

    models debugging

--- model/User.js
+++ model/User.js
@@ -1,46 +1,44 @@
-const { Schema, model, Types } = require('mongoose');
-const dateFormat = require('../utils/dateFormat');
+const { Schema, model } = require('mongoose');
 
-const userSchema = new Schema(
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+    match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, 'Please Enter a valid email address']
+  },
+  thoughts: [
     {
-        username: {
-            type: String,
-            trim:true,
-            required:true,
-            unique:true
-        },
-        email: {
-            type: String,
-            trim:true,
-            required:true,
-            match: [/.+\@.+\..+/, 'Enter valid email format']
-        },
-        thoughts: [{
-            type: Schema.Types.ObjectId,
-            ref: 'Thought'
-
-        }],
-        friends: [{
-            type: Schema.Types.ObjectId,
-            ref: 'User'
-
-        }]
+      type: Schema.Types.ObjectId,
+      ref: 'Thought'
+    }
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ]
+  },
+  {
+    toJSON: {
+      virtuals: true
     },
+    id: false
+  }
+);
 
-    {
-        toJSON: {
-          virtuals: true,
-          getters: true
-        },
-        id: false
-      });
+UserSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+});
 
-      //Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+const User = model('User', UserSchema);
 
-      userSchema.virtual('friendCount').get(function() {
-        return this.friends.length;
-      });
-      
-      const User = model('User', userSchema);
-      
-      module.exports = User;
+module.exports = User;
